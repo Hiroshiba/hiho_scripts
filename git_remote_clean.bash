0a26bcc10a006b03be199c9d2d6304e448a597b6
Hiroshiba <hihokaruta@gmail.com>
2025-01-18 21:36:49 +0900

    add git_remote_clean

---
+++ git_remote_clean.bash
@@ -0,0 +1,19 @@
+# git remote のうち origin と upstream 以外を削除する
+
+function git_remote_clean() {
+  # 必要なコマンドがあるか確認
+  if ! command -v git >/dev/null; then
+    echo "エラー: gitコマンドが見つかりません。gitコマンドをインストールしてください。" >&2
+    return 1
+  fi
+
+  # リモート名を取得
+  local remotes
+  remotes=$(git remote)
+  for remote in $remotes; do
+    if [ "$remote" != "origin" ] && [ "$remote" != "upstream" ]; then
+      echo "リモート $remote を削除します..."
+      git remote remove "$remote"
+    fi
+  done
+}
